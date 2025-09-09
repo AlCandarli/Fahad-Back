9809c5e59a1cadef9c924bb6a08116b8ad2ca953
AHMED MUSHTAQ <www.amnesty55@gmail.com>
2025-09-09 04:50:45 +0300

    Update allowedOrigins.js

--- Config/allowedOrigins.js
+++ Config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["https://alcandarli.github.io/Fahad-Front/" , "https://fahad-front.vercel.app/", "http://localhost:5000"];
+const allowedOrigins = ["https://alcandarli.github.io/Fahad-Front/" , "https://fahad-front.vercel.app", "http://localhost:5000"];
 
 module.exports = allowedOrigins;
